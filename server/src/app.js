2746f3474e668f5b7ba218646a81cc2e4aebcaf1
Alberto Saenz <albertosaenz@Albertos-MacBook-Pro.local>
2022-12-27 18:02:56 -0500

    add req logger morgan to use with middleware and log request information

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const path = require('path')
+const morgan = require('morgan')
 
 const planetsRouter = require('./routes/planets/planets.router')
 
@@ -10,6 +11,10 @@ const app = express()
 app.use(cors({
     origin: 'http://localhost:3000',
 }))
+
+// using a request logger to log information about the requests
+app.use(morgan('combined'))
+
 app.use(express.json())
 app.use(planetsRouter)
 // Afeter adding the front end to out backend folder with the build script in client
