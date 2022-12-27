c8c6ded56ef0726676d044c79094ebbe8bde64a2
Alberto Saenz <albertosaenz@Albertos-MacBook-Pro.local>
2022-12-27 17:06:52 -0500

    add deploy script to build client and start server concurrently

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 
 const planetsRouter = require('./routes/planets/planets.router')
 
@@ -11,5 +12,8 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(planetsRouter)
+// Afeter adding the front end to out backend folder with the build script in client
+// we can serve it directly from our backend using middleware.
+app.use(express.static(path.join(__dirname, "..", "public")))
 
 module.exports = app
