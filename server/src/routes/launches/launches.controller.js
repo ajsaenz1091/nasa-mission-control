75171d345b56360a896e2f79d3d44a0fc4463a23
Alberto Saenz <albertosaenz@Albertos-MacBook-Pro.local>
2022-12-28 22:41:59 -0500

    add validation to launches post, connected launches request in front-end to back-end

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -10,7 +10,21 @@ function httpGetAllLaunches(req, res) {
 // POST Launch
 function httpAddNewLaunch(req, res) {
     const launch = req.body
+    // Manual validation for missing properties in request body
+    if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.destination) {
+        return res.status(400).json({
+            error: 'Missing required launch property',
+        })
+    }
+
     launch.launchDate = new Date(launch.launchDate)
+    // Date validation, isNaN converts date types to a number so the return value is "false" meaning that the date was successfully parsed and the code block should not execute
+    // On the other hand, any value that is not a date, will return NaN, meaning that the date was not successfully parsed and an error should be returned.
+    if (isNaN(launch.launchDate)) {
+        return res.status(400).json({
+            error: `Invalid launch date. Date should follow this format: January 17, 2030`
+        })
+    }
 
     addNewLaunch(launch)
 
