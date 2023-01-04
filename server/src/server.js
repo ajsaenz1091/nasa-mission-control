f3b1607645625b210ceb2d15684fe99ddbc009ec
Alberto Saenz <albertosaenz@Albertos-MacBook-Pro.local>
2023-01-03 19:22:50 -0500

    add and connect mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http =  require('http')
+const mongoose = require('mongoose')
 
 const app = require('./app')
 
@@ -6,10 +7,26 @@ const { loadPlanetsData } = require('./models/planets.model')
 
 const PORT = process.env.PORT || 8000
 
+const MONGO_URL = 'mongodb+srv://nasa-api:MBQlMqLxWt17ss3j@nasacluster.cyumuee.mongodb.net/nasa?retryWrites=true&w=majority'
+
 const server = http.createServer(app)
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!')
+})
+
+mongoose.connection.on('error', (err) => {
+    console.log(err)
+})
+
+mongoose.set('strictQuery', true)
 
 // We want the planets to be loaded before any request is made to the server so we use await
 async function startServer() {
+    await mongoose.connect(MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     await loadPlanetsData()
     server.listen(PORT, () => {
         console.log(`Listening on port ${PORT}...`)
