6e7df1a5ae01a35004dd692d5dbbdaf3b5957c3d
Alberto Saenz <albertosaenz@Albertos-MacBook-Pro.local>
2023-01-10 13:03:28 -0500

    sort launches by flight number

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -134,6 +134,7 @@ async function getAllLaunches(skip,limit) {
     return await Launch.find({},{
         '_id': 0, '__v': 0,
     })
+    .sort({flightNumber: 1})
     .skip(skip)
     .limit(limit)
 }
