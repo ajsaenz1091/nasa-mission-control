ba52e4b1bebb221f89cee0c7492af442edc87c42
Alberto Saenz <albertosaenz@Albertos-MacBook-Pro.local>
2023-02-02 20:50:41 -0500

    fix mongo disconnect

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,29 +1,30 @@
 const mongoose = require('mongoose')
 
+
+const mongoose = require('mongoose');
+
 require('dotenv').config();
 
-const MONGO_URL = process.env.MONGO_URL
-
-
+// Update below to match your own MongoDB connection string.
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
-    console.log('MongoDB connection ready!')
-})
+  console.log('MongoDB connection ready!');
+});
 
 mongoose.connection.on('error', (err) => {
-    console.log(err)
-})
-
-mongoose.set('strictQuery', true)
+  console.error(err);
+});
 
 async function mongoConnect() {
-    await mongoose.connect(MONGO_URL)
+  await mongoose.connect(MONGO_URL);
 }
 
-// async function mongoDisconnect() {
-//     await mongoose.disconnect()
-// }
+async function mongoDisconnect() {
+  await mongoose.disconnect();
+}
 
 module.exports = {
-    mongoConnect,
+  mongoConnect,
+  mongoDisconnect,
 }
